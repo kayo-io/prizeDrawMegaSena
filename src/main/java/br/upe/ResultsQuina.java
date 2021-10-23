bacf97b95d337ef751ca931a4bb728702767d031
Kayo Renato <kayorenatocontato@gmail.com>
2021-10-23 14:13:04 -0300

    :sparkles: Get last Result Quina of prize draw Lottery.

---
+++ src/main/java/br/upe/ResultsQuina.java
@@ -0,0 +1,55 @@
+package br.upe;
+
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ResultsQuina {
+    private final static String URL = "http://loterias.caixa.gov.br/wps/portal/loterias/landing/";
+
+    private final static String initialTag = "<ul class=\"resultado-loteria quina\">";
+
+    private final static String finalTag = "</ul>";
+
+    public static List<String> getLastResult() {
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        try {
+            HttpGet httpget = new HttpGet(URL);
+
+            String html = httpClient.execute(httpget, new BasicResponseHandler());
+
+            return getDozens(html);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            httpClient.getConnectionManager().shutdown();
+
+        }
+
+        return null;
+    }
+
+    private static List<String> getDozens(String html) {
+        int idxBeginSearch = html.indexOf(initialTag) + initialTag.length();
+
+        int idxEndSearch = idxBeginSearch + html.substring(idxBeginSearch,html.length()).indexOf(finalTag);
+
+        String extract = html.substring(idxBeginSearch, idxEndSearch);
+
+        String[] trashNumbers = extract.split("</li>");
+        List<String> numbers = Arrays.stream(trashNumbers).map(trash -> trash.replace("<li>", "")).collect(Collectors.toList());
+
+        return numbers.subList(0,5);
+
+    }
+
+}
